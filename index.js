5ffe40114bdc9bb15f92d6ebc039e84ba343a36a
Walker <70336870+walkerofthewoods@users.noreply.github.com>
2020-10-11 15:02:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ const runComparison = () => {
     leftSideStats.forEach((leftStat, index) => {
         const rightStat = rightSideStats[index];
 
-        const leftSideValue = parseInt(leftStat.dataset.value);
-        const rightSideValue = parseInt(rightStat.dataset.value);
+        const leftSideValue = parseFloat(leftStat.dataset.value);
+        const rightSideValue = parseFloat(rightStat.dataset.value);
 
         if (rightSideValue > leftSideValue) {
             leftStat.classList.remove('is-primary');
